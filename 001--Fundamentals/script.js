2c54841586c40d67cc5bb703df3c0cd224776a4c
huyphamfc <huyphamfc@gmail.com>
2022-04-17 08:43:03 +0700

    Fundamentals - update switch Statement

--- 001--Fundamentals/script.js
+++ 001--Fundamentals/script.js
@@ -110,3 +110,31 @@ if (shouldDrive) {
 } else {
     console.log('Someone else should drive ...');
 }
+
+//switch statement
+const day = 'sun';
+switch (day) {
+    case 'mon':
+        console.log('Monday');
+        break;
+    case 'tue':
+        console.log('Tuesday');
+        break;
+    case 'wed':
+        console.log('Wednesday');
+        break;
+    case 'thu':
+        console.log('Thursday');
+        break;
+    case 'fri':
+        console.log('Friday');
+        break;
+    case 'sat':
+        console.log('Saturday');
+        break;
+    case 'sun':
+        console.log('Sunday');
+        break;
+    default:
+        console.log('Not a valid day');
+}
