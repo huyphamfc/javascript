06c26729d209a796db4d190fe83eaaa6025feea7
huyphamfc <huyphamfc@gmail.com>
2022-05-02 13:50:50 +0700

    How JS Works - Hoisting & TDZ

--- 006--How-JS-Works/script.js
+++ 006--How-JS-Works/script.js
@@ -21,6 +21,43 @@ function calcAge(birthYear) {
     }
     printAge();
     return age;
-}
-const firstName = 'Baby';
-calcAge(2020);
+
+
+    // HOISTING & TDZ
+    console.log(variable); // variable is not defined
+    console.log(me); // undefined
+    console.log(job); // Cannot access 'job' before initialization
+    console.log(year); // Cannot access 'year' before initialization
+    var me = 'Baby';
+    let job = 'Dev';
+    const year = 2020;
+
+    console.log(addDeclaration(1, 2)); // OK
+    console.log(addExpression(1, 2)); // Cannot access 'addExpression' before initialization
+    console.log(addArrow(1, 2)); // Cannot access 'addArrow' before initialization
+    console.log(addArrow_2(1, 2)); // 'addArrow_2' is not a function
+    function addDeclaration(a, b) {
+        return a + b;
+    }
+    const addExpression = function (a, b) {
+        return a + b;
+    }
+    const addArrow = (a, b) => {
+        a + b;
+    }
+    var addArrow_2 = (a, b) => {
+        a + b;
+    }
+
+    if (!numProducts) deleteShoppingCart(); // All products deleted
+    var numProducts = 10;
+    function deleteShoppingCart() {
+        console.log('All products deleted');
+    }
+
+    var x = 1;
+    const y = 2;
+    let z = 3;
+    console.log(x === window.x); // true
+    console.log(y === window.y); // false
+    console.log(z === window.z); // false
