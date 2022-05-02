20d56c87bd93e2a14d752b99e5ee3252cb2b6a75
huyphamfc <huyphamfc@gmail.com>
2022-05-02 11:08:28 +0700

    How JS Works - Scope

---
+++ 006--How-JS-Works/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// SCOPE
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+    function printAge() {
+        let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+        console.log(output);
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+            const firstName = 'Steven';
+            output = 'NEW OUTPUT!';
+            const str = `Oh, and you're a millenial, ${firstName}`;
+            console.log(str);
+            function add(a, b) {
+                return a + b;
+            }
+        }
+        console.log(millenial);
+        console.log(output);
+    }
+    printAge();
+    return age;
+}
+const firstName = 'Baby';
+calcAge(2020);
