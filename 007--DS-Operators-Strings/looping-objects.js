6350dc2ef4bc4ecdc8f14f910dda812e7fe0791f
huyphamfc <huyphamfc@gmail.com>
2022-05-10 10:29:03 +0700

    DS-Operators-Strings - Looping Objects

---
+++ 007--DS-Operators-Strings/looping-objects.js
@@ -0,0 +1,45 @@
+'use strict'
+
+
+const openingHours = {
+    thu: { open: 12, close: 22 },
+    fri: { open: 11, close: 23 },
+    sat: { open: 0, close: 24 }
+}
+
+for (const day of Object.keys(openingHours)) {
+    console.log(day); // thu fri sat
+}
+
+
+// PROPERTY NAMES
+const properties = Object.keys(openingHours);
+console.log(properties); // [ 'thu', 'fri', 'sat' ]
+
+
+// PROPERTY VALUES
+const values = Object.values(openingHours);
+console.log(values);
+// [ { open: 12, close: 22 }, { open: 11, close: 23 }, { open: 0, close: 24 } ]
+
+
+// ENTRY OBJECTS
+const entries = Object.entries(openingHours);
+console.log(entries);
+/**
+[
+  [ 'thu', { open: 12, close: 22 } ],
+  [ 'fri', { open: 11, close: 23 } ],
+  [ 'sat', { open: 0, close: 24 } ]
+]
+ */
+
+// [key, value]
+for (const [key, { open, close }] of entries) {
+    console.log(`On ${key}, we open at ${open} and close at ${close}.`);
+}
+/**
+On thu, we open at 12 and close at 22.
+On fri, we open at 11 and close at 23.
+On sat, we open at 0 and close at 24.
+ */
