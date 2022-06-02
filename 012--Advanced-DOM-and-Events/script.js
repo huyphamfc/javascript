02d0fd4f347a480cc1fb147193c4e982c256e827
huyphamfc <huyphamfc@gmail.com>
2022-06-02 11:31:27 +0700

    Bankist - implement Page Navigation

--- 012--Advanced-DOM-and-Events/script.js
+++ 012--Advanced-DOM-and-Events/script.js
@@ -42,3 +42,20 @@ btnScrollTo.addEventListener('click', e => {
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+
+// PAGE NAVIGATION
+// document.querySelectorAll('.nav__link').forEach(function (element) {
+//   element.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
