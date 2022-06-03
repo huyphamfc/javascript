d610f625ad681f585da74f54c6d1eb14cab3fdfc
huyphamfc <huyphamfc@gmail.com>
2022-06-03 10:42:39 +0700

    Bankist - implement the Tabbed Component

--- 012--Advanced-DOM-and-Events/script.js
+++ 012--Advanced-DOM-and-Events/script.js
@@ -5,8 +5,13 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnCloseModal = document.querySelector('.btn--close-modal');
+
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 
 // MODAL
@@ -57,5 +62,20 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+
+// TABBED COMPONENT
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (clicked) {
+    tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+    tabsContent.forEach(content => content.classList.remove('operations__content--active'));
+
+    clicked.classList.add('operations__tab--active');
+    document
+      .querySelector(`.operations__content--${clicked.dataset.tab}`)
+      .classList.add('operations__content--active');
   }
 });
