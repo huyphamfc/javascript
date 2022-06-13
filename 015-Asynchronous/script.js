a99ec0546d5141160aa7cb1f811d08460d8a53c1
huyphamfc <huyphamfc@gmail.com>
2022-06-13 14:05:43 +0700

    Asynchronous - implement XMLHttpRequest()

--- 015-Asynchronous/script.js
+++ 015-Asynchronous/script.js
@@ -4,6 +4,23 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 
+const render = function (data) {
+    const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${Object.values(data.languages)}</p>
+            <p class="country__row"><span>💰</span>${Object.keys(data.currencies)}</p>
+        </div>
+    </article>
+    `
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = '1';
+}
+
 const getCountryData = function (country) {
     const request = new XMLHttpRequest();
     request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
@@ -12,24 +29,19 @@ const getCountryData = function (country) {
     request.addEventListener('load', function () {
         const [data] = JSON.parse(this.responseText);
         // console.log(data);
-
-        const html = `
-        <article class="country">
-            <img class="country__img" src="${data.flags.svg}" />
-            <div class="country__data">
-                <h3 class="country__name">${data.name.common}</h3>
-                <h4 class="country__region">${data.region}</h4>
-                <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}M people</p>
-                <p class="country__row"><span>🗣️</span>${Object.values(data.languages)}</p>
-                <p class="country__row"><span>💰</span>${Object.keys(data.currencies)}</p>
-            </div>
-        </article>
-        `
-        countriesContainer.insertAdjacentHTML('beforeend', html);
-        countriesContainer.style.opacity = '1';
+        render(data);
     });
 }
 
-getCountryData('Vietnam');
-getCountryData('USA');
-getCountryData('Germany');
+const getCountryAndNeighbor = function (country) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+    request.addEventListener('load', function () {
+        const [data] = JSON.parse(this.responseText);
+        render(data);
+        data.borders?.forEach(item => getCountryData(item));
+    });
+}
+
+getCountryAndNeighbor('USA');
