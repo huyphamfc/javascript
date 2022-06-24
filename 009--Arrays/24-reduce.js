113afa319c1adce3a2bcb9ebe57e800465462572
huyphamfc <huyphamfc@gmail.com>
2022-06-24 09:15:19 +0700

    Arrays - How reduce() works (fixed)

--- 009--Arrays/24-reduce.js
+++ 009--Arrays/24-reduce.js
@@ -3,12 +3,23 @@
 
 const arr = [1, 2, 3, 4, 5];
 
-Array.prototype.reduceHuyPham = function (callbackFunction, previous = this[0]) {
-    const lengthOfArray = this.length;
-    let result = previous;
-    for (let i = 0; i < lengthOfArray; i++)
-        result = callbackFunction(result, this[i], i, this);
-    return result;
+Array.prototype.reduceHuyPham = function (callbackFunction, initialValue) {
+    let lengthOfArray = this.length;
+    let previousValue;
+    let i;
+
+    if (initialValue) {
+        i = 0;
+        previousValue = initialValue;
+    } else {
+        i = 1;
+        previousValue = this[0];
+    }
+
+    for (i; i < lengthOfArray; i++)
+        previousValue = callbackFunction(previousValue, this[i], i, this)
+    return previousValue;
 }
 
-console.log(arr.reduceHuyPham((sum, value) => sum + value, 10));
+console.log(arr.reduceHuyPham(((max, value) => value > max ? value : max), 10));
+console.log(arr.reduce(((max, value) => value > max ? value : max), 10));
