7f9662c5d3da15279070d50f7968ab647263035c
huyphamfc <huyphamfc@gmail.com>
2022-07-27 10:08:54 +0700

    Todolist - build template view function

--- Section--12--Todo-List/core.js
+++ Section--12--Todo-List/core.js
@@ -1,3 +1,11 @@
-export default function html() {
-
+export default function html([first, ...strings], ...values) {
+    return values
+        .reduce(
+            (acc, cur) => acc.concat(cur, strings.shift()),
+            [first]
+        )
+        .filter(item =>
+            item && item !== true || item === 0
+        )
+        .join('');
 }
