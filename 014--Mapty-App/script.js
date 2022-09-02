61ecbfb5219e06c97689a0ced634933fb0d6f93a
huyphamfc <huyphamfc@gmail.com>
2022-09-02 17:41:45 +0700

    Mapty - save Workouts to the localStorage

--- 014--Mapty-App/script.js
+++ 014--Mapty-App/script.js
@@ -85,12 +85,15 @@ class App {
     #workouts = [];
 
     constructor() {
+        // get user's position
         this._getPosition();
 
+        // get data from local storage
+        this._getLocalStorage();
+
+        // attach event handlers
         form.addEventListener('submit', this._newWorkout.bind(this));
-
         inputType.addEventListener('change', this._toggleElevationField.bind(this));
-
         containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
     }
 
@@ -189,6 +192,9 @@ class App {
 
         // hide form and clear input fields
         this._hideForm();
+
+        // set local storage to all workouts
+        this._setLocalStorage();
     }
 
     _renderWorkoutMarker(workout) {
@@ -272,7 +278,25 @@ class App {
         });
 
         // using the public interface
-        workout.click();
+        // workout.click();
+    }
+
+    _setLocalStorage() {
+        localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+    }
+
+    _getLocalStorage() {
+        const data = JSON.parse(localStorage.getItem('workouts'));
+
+        if (!data) return;
+
+        this.#workouts = data;
+        this.#workouts.forEach(workout => this._renderWorkout(workout));
+    }
+
+    reset() {
+        localStorage.removeItem('workouts');
+        location.reload();
     }
 }
 
